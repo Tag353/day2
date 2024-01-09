30f2fb99287af04768b2536a5ebacde43b7c08d6
Tage Cerny <tagecerny@Tages-MacBook-Pro.local>
2024-01-09 09:35:49 -0700

    first commit

---
+++ project.sh
@@ -0,0 +1,37 @@
+cd ~Tages-MacBook-Pro day one basic os basic os
+git clone #repo name here
+#cd #repo from step 2
+
+
+
+mkdir webDev
+cd webDev
+mkdir projects
+cd projects
+mkdir sampleProject
+cd sampleProject
+touch index.html
+mkdir css
+cd css
+touch style.css
+mkdir pages
+cd pages
+mkdir about
+cd about
+touch about.html
+touch about.css
+mkdir contact
+cd contact
+touch contact.html
+touch contact.css
+mkdir images
+cd images
+touch companyLogo.png
+mkdir icons
+cd icons
+touch email.png
+touch phone.png
+touch chat.png
+mkdir js
+cd js
+touch index.js
